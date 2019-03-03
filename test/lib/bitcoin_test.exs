7e6695a797ea0c3af265ac702a2e040af803b0f1
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2019-03-03 09:41:54 -0500

    less noise

--- test/lib/bitcoin_test.exs
+++ test/lib/bitcoin_test.exs
@@ -1,14 +1,15 @@
 defmodule ElixirBlockchainBlockTest do
   use ExUnit.Case
+  alias ElixirBlockchain.Bitcoin, as: Bitcoin
   doctest ElixirBlockchain.Bitcoin
 
   test "default values" do
-    assert %ElixirBlockchain.Bitcoin{}.bits == ''
-    assert %ElixirBlockchain.Bitcoin{}.version == 0
-    assert %ElixirBlockchain.Bitcoin{}.merkel_root == ''
-    assert %ElixirBlockchain.Bitcoin{}.timestamp == ''
-    assert %ElixirBlockchain.Bitcoin{}.nonce == ''
-    assert %ElixirBlockchain.Bitcoin{}.previous_block == ''
+    assert %Bitcoin{}.bits == ''
+    assert %Bitcoin{}.version == 0
+    assert %Bitcoin{}.merkel_root == ''
+    assert %Bitcoin{}.timestamp == ''
+    assert %Bitcoin{}.nonce == ''
+    assert %Bitcoin{}.previous_block == ''
   end
 
   test "genesis block" do
